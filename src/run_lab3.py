3db2ca8c6ad59cd678cdbf4a7eecfa7fba60c0eb
randydapogi <randy.beros@gmail.com>
2026-02-20 23:40:40 +0800

    challenge 1: change print statement to print point.as_dict() and added try except, run_lab.py uncommented all challenges

--- src/run_lab3.py
+++ src/run_lab3.py
@@ -1,36 +1,46 @@
 from shapely.geometry import Polygon 
 from spatial import Point, Parcel
 
-# # Challenge 1
-# print("Challenge 1")
-# row1 = {"id": "A", "lon": 121.0, "lat": 14.6, "name": "Gate", "tag": "POI"}
-# row2 = {"id": "B", "lon": 999, "lat": 14.6, "name": "Gate", "tag": "POI"}
+# Challenge 1
+print("Challenge 1")
+row1 = {"id": "A", "lon": 121.0, "lat": 14.6, "name": "Gate", "tag": "POI"}
+row2 = {"id": "B", "lon": 999, "lat": 14.6, "name": "Gate", "tag": "POI"}
+try:
+    print(f"Creating Point with ID: {row1['id']} Lat: {row1['lat']} Lon: {row1['lon']}")
+    p1 = Point.from_dict(row1)
+    print(f"Output: {p1.as_dict()}")
+except ValueError as e:
+    print(f"Error: {e}")
+    
+try:
+    print(f"Creating Point with ID: {row2['id']} Lat: {row2['lat']} Lon: {row2['lon']}")
+    p2 = Point.from_dict(row2)
+    print(f"Output: {p2.as_dict()}")
+except ValueError as e:
+    print(f"Error: {e}")
 
-# p1 = Point.from_dict(row1)
-# print(p1)
-# p2 = Point.from_dict(row2)
-# print(p2)
 
+# Challenge 2
+print("\n\nChallenge 2")
+point_row = {"id": "A", "lon": 121.0, "lat": 14.6, "name": "Gate", "tag": "POI"}
+point = Point.from_dict(point_row)
+print("Printing Point")
+print(f"Output: {point.as_dict()}")
 
-# # Challenge 2
-# print("\n\nChallenge 2")
-# point_row = {"id": "A", "lon": 121.0, "lat": 14.6, "name": "Gate", "tag": "POI"}
-# point = Point.from_dict(point_row)
-# print(point.as_dict())
-
-# attrs = { 
-#         "area": 50.0, 
-#         "zone": "Residential", 
-#         "is_active": True 
-#     } 
-# geom = Polygon([ 
-#         (0, 0), 
-#         (10, 0), 
-#         (10, 5), 
-#         (0, 5) 
-#     ])
-# parcel = Parcel(parcel_id=101, geometry=geom, attributes=attrs) 
-# print(parcel.as_dict())
+attrs = { 
+        "area": 50.0, 
+        "zone": "Residential", 
+        "is_active": True 
+    } 
+geom = Polygon([ 
+        (0, 0), 
+        (10, 0), 
+        (10, 5), 
+        (0, 5) 
+    ])
+parcel = Parcel(parcel_id=101, geometry=geom, attributes=attrs) 
+print("Printing Parcel")
+print(f"Output: {parcel.as_dict()}")
 
 
 # Challenge 3
